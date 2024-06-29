d6918b3fd26b73a28448fb21c524dd0e5ddf285e
againDDM <againddm@gmail.com>
2024-06-29 13:07:44 +0300

    Added logging

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ based on https://trstringer.com/quick-and-easy-prometheus-exporter/
 
 import os
 import time
+import logging
 from prometheus_client import start_http_server, CollectorRegistry, Enum
 import requests
 
@@ -32,6 +33,7 @@ class StaytusExporter:
         )
 
     def run(self):
+        logging.info("Run exporter.")
         start_http_server(self.exporter_port, registry=self.registry)
         self.run_metrics_loop()
 
@@ -42,26 +44,34 @@ class StaytusExporter:
             time.sleep(self.polling_interval_seconds)
 
     def fetch(self):
-        resp = requests.get(
+        "Metrics fetching method"
+        logging.debug("Try to update metrics.")
+        response = requests.get(
             url=f"{self.staytus_api_url}api/v1/services/all",
             headers={
                 "X-Auth-Token": self.staytus_api_token,
                 "X-Auth-Secret": self.staytus_api_secret,
             },
         )
-        resp.raise_for_status()
-        staytus_data = resp.json()
+        response.raise_for_status()
+        staytus_data = response.json()
         if staytus_data.get("status") != "success":
             raise ValueError("invalid staytus_data.status")
+        logging.debug("Success got staytus data.")
         for service_data in staytus_data["data"]:
             service_permalink = service_data["permalink"]
             service_status = service_data["status"]["status_type"]
             current_label = self.service_statuses_metric.labels(service_permalink=service_permalink)
             current_label.state(service_status)
+        logging.debug("Success updated metrics.")
 
 
 def main():
     "Main entry point"
+    logging.basicConfig(
+        level=logging.DEBUG if os.getenv("DEBUG") else logging.INFO,
+        format="%(asctime)s [%(levelname)s] <%(name)s> %(message)s",
+    )
     StaytusExporter().run()
 
 
